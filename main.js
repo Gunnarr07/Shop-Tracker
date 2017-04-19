d58ee55ac0bce31e1cf95e42ff793a4e75d343ae
Gunnarr07 <jeremyrt88@gmail.com>
2017-04-19 14:15:36 -0500

    work on setting up app

---
+++ main.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const {app, BrowserWindow, ipcMain} = require('electron')
+// const babel = require('babel-core')
+// import {transform} from 'babel-core'
+// import * as babel from 'babel-core'
+require('babel-core').transform('src/renderer/index/index')
+// babel.transform()
+
+let windows = []
+
+function createWindow () {
+  windows.push(new BrowserWindow())
+  let win = windows[windows.length - 1]
+  win.loadURL(`file://${__dirname}/public/index.html`)
+  win.webContents.openDevTools()
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+  if (windows.length === 0) {
+    createWindow()
+  }
+})
+
+app.on('browser-window-created', (event, window) => {
+  window.setMenu(null)
+})
---
+++ src/renderer/index/index.jsx
@@ -0,0 +1,23 @@
+// 'use babel'
+// const babel = require('babel-core')
+// import {transform} from 'babel-core'
+// // import * as babel from 'babel-core'
+// // require("babel-core").transform("code", options)
+// babel.transform()
+// const React = require('react')
+// const ReactDOM = require('react-dom')
+import React from 'react'
+import ReactDOM from 'react-dom'
+console.log(React)
+console.log(ReactDOM)
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+)
+
+// ReactDOM.render(
+//   React.createElement(
+//     'h1',
+//     'Hello World'
+//   )
+// )
